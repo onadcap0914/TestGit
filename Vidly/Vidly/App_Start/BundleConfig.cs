5130a1f4322994f55ede9433cedaf7989bab29a3
onadcap0914 <onadcap0914@gmail.com>
2018-05-19 16:59:03 +0800

    removed bootstrap-lumen.css

--- Vidly/Vidly/App_Start/BundleConfig.cs
+++ Vidly/Vidly/App_Start/BundleConfig.cs
@@ -22,13 +22,13 @@ namespace Vidly
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
-            //bundles.Add(new StyleBundle("~/Content/css").Include(
-            //          "~/Content/bootstrap.css",
-            //          "~/Content/site.css"));
-            
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap-lumen.css",
+                      "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            //bundles.Add(new StyleBundle("~/Content/css").Include(
+            //          "~/Content/bootstrap-lumen.css",
+            //          "~/Content/site.css"));
         }
     }
 }
